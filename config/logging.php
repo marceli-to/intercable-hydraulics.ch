1036a5216747da1311959bbec7186299f512de88
Marcel Stadelmann <m@marceli.to>
2022-06-29 13:24:04 +0200

    update logger

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single', 'slack'],
             'ignore_exceptions' => false,
         ],
 
@@ -55,11 +55,11 @@ return [
         ],
 
         'slack' => [
-            'driver' => 'slack',
-            'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => 'Laravel Log',
-            'emoji' => ':boom:',
-            'level' => 'critical',
+          'driver' => 'slack',
+          'url' => env('LOG_SLACK_WEBHOOK_URL'),
+          'username' => 'Laravel Log',
+          'emoji' => ':boom:',
+          'level' => env('LOG_LEVEL', 'critical'),
         ],
 
         'papertrail' => [
